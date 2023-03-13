703394aa841ad9c7c6fe96f893a9de74ffe26fe6
Joaquin Urbano Serra <kimbalear@gmail.com>
2023-03-13 19:27:49 +0100

    update form

--- scripts/main.js
+++ scripts/main.js
@@ -57,21 +57,55 @@ $(document).ready(function () {
         }
     });
 
-    // Form ansewer Show hide
-    $('input[type="radio"]').click(function () {
+    // Form 
+    $("#GBV").on("click", "input[type='radio']", function () {
         if ($(this).is(':checked')) {
             var value = $(this).val();
+            //console.log('Radio button seleccionado: ' + value);
             rbselected(value);
         }
     });
 
+    var oneyes = '<div id="oneyes" class="grdTb2-a1s"><div class="l1 lv1">If yes, did your clinic provide emergency contraception to survivors during this reporting period?</div><div class="ans"><input type="radio" name="q2" value="oneyes_y"><label for="Yes">Yes</label><input type="radio" name="q2" value="oneyes_n"><label for="No">No</label></div></div>'
+
+    var oneyes_n = '<div id="oneyes-n" class="grdTb2-a4"><div class="l1 lv1">If no, why is this service not available?</div><div class="ansa"><input type="radio" name="q3" value="oneyes-n1"><label for="q31">National policy does not allow</label></div><div class="ansb"><input type="radio" name="q3" value="oneyes-n2"><label for="q32">No stock available</label></div><div class="ansc"><input type="radio" name="q3" value="oneyes-n3"><label for="q33">No trained staff</label></div><div class="ansd"><input type="radio" name="q3" value="oneyes-n4"><label for="ecnoq34">Other:</label><input /></div></div>'
+
+    var oneno = '<div id="oneno" class="grdTb2-a1s"><div class="l1 lv1">If no, can your clinic support survivors to access emergency contraception elsewhere through a safe referral?</div><div class="ans">      <input type="radio" name="q4" value="onenoyes"><label for="Yes">Yes</label><input type="radio" name="q4" value="onenono"><label for="No">No</label></div></div>'
+
+    var oneno_n = '<div id="oneno-n" class="grdTb2-a2"><div class="l1 lv3">If no, Why?</div><div class="ans1"><input type="radio" name="q5" value="oneno-n1"><label for="q5">No one in the area provides this service</label></div><div class="ans2"><input type="radio" name="q5" value="oneno-n2"><label for="q5">Other:</label><input /></div></div>'
+
+    var two = '<div id="two" class="grdTb2-a1"><div class="l1 lv0">Is your clinic set up to provide Post-Exposure Prophylaxis for HIV to survivors?</div><div class="ans"><input type="radio" name="q6" value="twoYes"><label for="Yes">Yes</label><input type="radio" name="q6" value="twoNo"><label for="No">No</label></div></div>'
+
+    var twoyes = '<div id="twoyes" class="grdTb2-a1s"><div class="l1 lv1">If yes, did your clinic provide Post-Exposure Prophylaxis for HIV to survivors during this reporting period?</div><div class="ans"><input type="radio" name="q7" value="twoyes-y"><label for="Yes">Yes</label><input type="radio" name="q7" value="twoyes-n"><label for="No">No</label> </div></div>'
+
+    var twoyes_n = '<div id="twoyes-n" class="grdTb2-a4"><div class="l1 lv1">If no, why is this service not available?</div><div class="ansa"> <input type="radio" name="q8" value="twoyes-n1"><label for="q81">National policy does not allow</label></div><div class="ansb"><input type="radio" name="q8" value="twoyes-n2"><label for="q82">No stock available</label></div><div class="ansc"><input type="radio" name="q8" value="twoyes-n3"><label for="q83">No trained staff</label></div><div class="ansd"><input type="radio" name="q8" value="twoyes-n4"><label for="q84">Other:</label><input /></div></div>'
+
+    var twono = '<div id="twono" class="grdTb2-a1"><div class="l1 lv2">If no, can your clinic support survivors to access Post-Exposure Prophylaxis for HIV elsewhere  through a safe referral?</div><div class="ans"><input type="radio" name="q9" value="twono-y"><label for="Yes">Yes</label><input type="radio" name="q9" value="twono-n"><label for="No">No</label></div></div>'
+
+    var twono_n = '<div id="twono-n" class="grdTb2-a2"><div class="l1 lv3">If no, Why?</div><div class="ans1"><input type="radio" name="q10" value="twono-n1"><label for="q10">No one in the area provides this service</label></div><div class="ans2"><input type="radio" name="q10" value="twono-n2"><label for="q10">Other:</label><input /></div></div>'
+
     function rbselected(rbsctd) {
         switch (rbsctd) {
-            case 'ecYes':
-                console.log('Radio button seleccionado: ' + rbsctd);
+            case 'oneyes':
+                if (!$("#oneyes").length) {
+                    $("#oneno").remove();
+                    $("#one").after(oneyes)
+                    console.log('Radio button seleccionado: ' + rbsctd);
+                }
                 break;
-            case 'ecNo':
-                console.log('Radio button seleccionado: ' + rbsctd);
+
+            case 'oneyes_n':
+                if ($("#oneyes").length) {
+                    $("#oneyes").after(oneyes_n)
+                    console.log('Radio button seleccionado: ' + rbsctd);
+                }
+                break;
+            case 'oneno':
+                if (!$("#oneno").length) {
+                    $("#oneyes").remove();
+                    $("#oneyes-n").remove();
+                    $("#one").after(oneno)
+                }
                 break;
         }
     }
